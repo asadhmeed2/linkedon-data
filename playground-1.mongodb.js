c95b97d3031f96fe1c3a53a4275a0015f8aa3ffb
newasad <asadhmeed2@gmail.com>
2023-12-29 21:23:41 +0200

    ex3

--- playground-1.mongodb.js
+++ playground-1.mongodb.js
@@ -14,4 +14,9 @@ use('test-db');
 //     firstName:1,salary:1,_id:0
 // }).sort({salary: -1}).limit(3)
 
+//ex3
+// db.linkedon.count({"currentCompany.name":"Walmart",
+//     salary: {$gte:7000}
+// })
+
 
